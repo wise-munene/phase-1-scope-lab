de744a4c04c08a93f9e544d9290a4ba21882c479
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-09-03 13:38:56 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName ="bob";
+function upperCaseCustomerName(){
+ customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+    return  bestCustomer="not bob";
+}
+function overwriteBestCustomer(){
+    return bestCustomer="maybe bob";
+}
+const leastFavoriteCustomer= "tom"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer= "mary";
+}
